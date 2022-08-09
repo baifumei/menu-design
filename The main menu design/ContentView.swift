b04c238b5b43c5e4f3267a0e036e1f41a715df68
baifumei <k_katerina_k@icloud.com>
2022-08-09 19:12:57 +0300

    Initial Commit

---
+++ The main menu design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The main menu design
+//
+//  Created by Екатерина К on 09.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The main menu design/The_main_menu_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_main_menu_designApp.swift
+//  The main menu design
+//
+//  Created by Екатерина К on 09.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct The_main_menu_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
